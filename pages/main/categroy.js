02a432ed5aad7676fb86712e2ab587b9a29a8490
竹北 <koyo527Les@gmail.com>
2018-10-27 13:56:16 +0800

    分类样式

--- pages/main/categroy.js
+++ pages/main/categroy.js
@@ -5,7 +5,8 @@ Page({
     navLeftItems: [], //左侧导航栏内容
     navRightItems: [], //右侧数据
     curNav: 1,
-    curIndex: 0 
+    curIndex: 0,
+    testData: [], //测试数据
   },
 
   onLoad: function(options) {
@@ -25,6 +26,9 @@ Page({
     })
     getProducts(this, id);
   },
+  /**
+   * 查询类型列表
+   */
   getDataList: function() {
     var that = this;
     app.request({
@@ -38,43 +42,10 @@ Page({
     });
   },
   /**
-   * 添加商品到购物车
+   * 商品详情
    */
-  addToCar: function(e) {
-    var currentData = e.currentTarget.dataset.item;
-    //显示加载框
-    wx.showLoading({
-      title: '加载中',
-    })
-    //根据wineId查询
-    app.request({
-      url: '/shoppingcar/queryList.do',
-      method: 'POST',
-      data: {
-        wineId: currentData.id,
-        createUser: app.globalData.userInfo.userId
-      },
-      success: function(res) {
-        var wineNumber = 1;
-        if (res.data.length>0) {
-          wineNumber = (res.data[0].number)*1+1;
-          updateShoppincar(res.data[0].id, wineNumber);
-        }else{
-          addCar(app.globalData.userInfo.userId, currentData.id, wineNumber)
-        }
-      },
-      error: function(err) {
-        //隐藏加载框
-        wx.hideLoading();
-        wx.showToast({
-          title: '服务器异常'
-        })
-      }
-    });
-
-  },
-  productionInfo:function(e){
-    var wineId = e.currentTarget.dataset.id; 
+  productionInfo: function(e) {
+    var wineId = e.currentTarget.dataset.id;
     wx.navigateTo({
       url: 'productInfo?id=' + wineId,
     })
@@ -85,74 +56,70 @@ Page({
  * 获取商品数据
  */
 function getProducts(that, categoryId) {
+  wx.showLoading({
+    title: '加载中',
+  })
   app.request({
     url: '/wine/queryPage.do',
     method: 'POST',
-    data: { 
+    data: {
       categroyId: categoryId,
-      page:0,
-      rows:20
+      page: 0,
+      rows: 20
     },
     success: function(res) {
-      console.log(res);
+      // console.log(res);
       that.setData({
         navRightItems: res.data.data
       });
+      wx.hideLoading();
+    //  init(that, res.data.data);
+    },
+    fail:function(err){
+      wx.hideLoading();
     }
   });
 }
 
-/**
- * 更新购物车数据
- */
-function updateShoppincar(id,numbers){
-  app.request({
-    url: '/shoppingcar/updateNumber.do',
-    method: 'POST',
-    data: {
-      id:id,
-      number:numbers
-    },
-    success: function (res) {
-      wx.hideLoading();
-      wx.showToast({
-        title: '已加入购物车'
-      })
-    },
-    error:function(err){
-      wx.hideLoading();
-      wx.showToast({
-        title: '服务器异常'
-      })
-    }
-  });
-    
+function init(that, navRightItems) {
+  var testData = [
+    {
+    name: '商城主打酒',
+      wineDate: [
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0]
+    ]
+  },
+  {
+    name: '备受欢迎',
+    wineDate: [
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0],
+      navRightItems[1]
+    ]
+  },
+  {
+    name: '茅台旗下酒',
+    wineDate: [
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0],
+      navRightItems[1],
+      navRightItems[0]
+    ]
+  }
+  ];
+  that.setData({
+    testData: testData
+  })
 }
-
-/**
- * 添加商品到购物车
- */
-function addCar(userId,wineId,numbers){
-  app.request({
-    url: '/shoppingcar/saveEntity.do',
-    method: 'POST',
-    data: {
-      createUser: userId,
-      wineId: wineId,
-      number: numbers
-    },
-    success: function (res) {
-      wx.hideLoading();
-      wx.showToast({
-        title: '已加入购物车'
-      })
-    },
-    error: function (err) {
-      wx.hideLoading();
-      wx.showToast({
-        title: '服务器异常'
-      })
-    }
-  });
-}
-
